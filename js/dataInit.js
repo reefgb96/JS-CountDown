2f7d287a82fdb5b144da5d5628fbd61e628962d0
reef goldberg <reef.goldberg904@gmail.com>
2022-08-30 14:14:24 +0300

    init-rep🚀

--- js/dataInit.js
+++ js/dataInit.js
@@ -2,21 +2,8 @@
 
 let countDownCardsArr = [
   new CountDownCard(
-    "React.JS",
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/800px-React.svg.png",
-    "september 11, 2022 09:03:45",
+    "Ericeira",
+    "september 29, 2022 05:10:00",
     1
-  ),
-  new CountDownCard(
-    "BackEnd",
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/320px-Node.js_logo.svg.png",
-    "november 10, 2022 09:05:30",
-    2
-  ),
-  new CountDownCard(
-    "Course Ending",
-    "https://www.hackeru.co.il/co/assets/images/hackeru_logo.png",
-    "december 12, 2022 01:01:20",
-    3
   ),
 ];
